dfb142a33760b1f1ac8dde8febd82db502841fad
Jagarlamudi Venkata Trinadh <trinadh2026@gmail.com>
2025-10-14 23:10:12 +0530

    app and demployment

---
+++ application.py
@@ -0,0 +1,43 @@
+import pickle
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+import sys
+
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+
+
+application=Flask(__name__) 
+app=application
+
+## route
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/predictdata', methods=['GET','POST'])
+def predict_datapoint():
+    if request.method == 'POST':
+        data=CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('race_ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=request.form.get('reading_score'),
+            writing_score=request.form.get('writing_score')
+        )
+        pred_df = data.get_data_as_dataframe()
+        print(pred_df)
+
+        predict_pipeline = PredictPipeline()
+        preds = predict_pipeline.predict(pred_df)
+        return render_template('home.html', results=preds[0])
+    return render_template('home.html')
+
+
+if __name__=="__main__":
+    app.run(debug=True)
